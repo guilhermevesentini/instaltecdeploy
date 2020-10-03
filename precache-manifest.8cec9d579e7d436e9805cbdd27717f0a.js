1744e3825d51bdf6a06b785cbbfd43c33e017d1f
Guilherme Vesentini <guilherme1992@hotmail.com>
2020-10-03 12:23:37 +0100

    Updates

---
+++ precache-manifest.8cec9d579e7d436e9805cbdd27717f0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b180024ea18f6126e91220c1e0d035cf",
+    "url": "/instaltecdeploy/index.html"
+  },
+  {
+    "revision": "fade5b43c206e4c86750",
+    "url": "/instaltecdeploy/static/css/main.b69f3d14.chunk.css"
+  },
+  {
+    "revision": "d34b368ec94ff53635e7",
+    "url": "/instaltecdeploy/static/js/2.9b9fbcbe.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/instaltecdeploy/static/js/2.9b9fbcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fade5b43c206e4c86750",
+    "url": "/instaltecdeploy/static/js/main.1d514b50.chunk.js"
+  },
+  {
+    "revision": "0a93bd96f41866e1bab2",
+    "url": "/instaltecdeploy/static/js/runtime-main.d3705cda.js"
+  },
+  {
+    "revision": "c6f559007c041f087c8d75a1047bd891",
+    "url": "/instaltecdeploy/static/media/Grupo6.c6f55900.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d38d115e4726564ca6faf8618c80fdbd.js"
+  "/instaltecdeploy/precache-manifest.8cec9d579e7d436e9805cbdd27717f0a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/instaltecdeploy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
