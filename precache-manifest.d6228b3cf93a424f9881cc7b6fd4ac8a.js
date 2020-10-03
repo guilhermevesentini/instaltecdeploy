beaf4dcb3ef8d5d04d61b822d44cc371c7b5ddcf
Guilherme Vesentini <guilherme1992@hotmail.com>
2020-10-03 12:14:20 +0100

    Updates

---
+++ precache-manifest.d6228b3cf93a424f9881cc7b6fd4ac8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e62d4302dc2ecfa213188b5cbd201d",
+    "url": "/instaltecdeploy/index.html"
+  },
+  {
+    "revision": "5f54cac9fd64f1baadf5",
+    "url": "/instaltecdeploy/static/css/main.b69f3d14.chunk.css"
+  },
+  {
+    "revision": "d34b368ec94ff53635e7",
+    "url": "/instaltecdeploy/static/js/2.9b9fbcbe.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/instaltecdeploy/static/js/2.9b9fbcbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f54cac9fd64f1baadf5",
+    "url": "/instaltecdeploy/static/js/main.e4ee3c02.chunk.js"
+  },
+  {
+    "revision": "0a93bd96f41866e1bab2",
+    "url": "/instaltecdeploy/static/js/runtime-main.d3705cda.js"
+  },
+  {
+    "revision": "c6f559007c041f087c8d75a1047bd891",
+    "url": "/instaltecdeploy/static/media/Grupo6.c6f55900.png"
+  }
+]);
